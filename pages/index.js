cd7a09ec80e98c871123f89bc3ccc58879ad628b
Elijah Ang <eang0521@users.noreply.github.com>
2020-08-07 05:02:10 +0000

    first commit :)

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,37 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Row from 'react-bootstrap/Row'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    return (
+        <>
+        <Head>
+            <link rel="icon" href="images/Logo.png" /><title>Awe & Reverence</title>
+        </Head>
+        <Navbar fixed= 'top' style={{ backgroundColor: 'rgba(200, 200, 200, 0)' }}>
+            <Navbar.Brand href="#home" style={{ fontFamily: 'TimesNewRoman', textAlign: 'center' }}>A w e &nbsp; &<br />R e v e r e n c e</Navbar.Brand>
+            <Nav className="ml-auto" style={{ fontFamily: 'TimesNewRoman', float: 'right', textAlign: 'right' }}>
+                <Nav.Link href="#about">ABOUT</Nav.Link>
+                <Nav.Link href="#blog">BLOG</Nav.Link>
+                <Nav.Link href="https://maskil.church" target="_blank">MASKIL</Nav.Link>
+                <Nav.Link href="#apprenticeship">APPRENTICESHIP</Nav.Link>
+            </Nav>
+        </Navbar>
+        <img src="https://via.placeholder.com/1750x1000.png" style={{ width: '100%' }} />
+        <br />
+        <br />
+        <Row>
+            <Col><img src="https://via.placeholder.com/800x600.png" style={{ width: '100%', display: 'flex' }} /></Col>
+            <Col><Container style={{ align: 'right' }}>
+                <h3 style={{ textAlign: 'center' }}>Apprenticeship</h3>
+                <p>Our founder, Jonathan Tsai, started an apprenticeship program at A&R to train and equip aspiring Christian software engineers with tangible skills. In the SF Bay Area there is an ever-increasing number of people coming out of coding bootcamps trying to transition into software roles from other industries. Unfortunately, opportunities for bootcamp grads have severely declined since 2015 and many people are struggling to find employment after investing substantially into building their coding skills</p>
+            </Container></Col>
+        </Row>
+        </>
+    )
 }
