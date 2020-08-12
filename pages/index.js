a359733e4dacff3edeb884d8f280e561c1af4206
Elijah Ang <eang0521@users.noreply.github.com>
2020-08-12 04:31:02 +0000

    more index.js changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
@@ -22,16 +23,26 @@ export default function Home() {
                 <Nav.Link href="#apprenticeship">APPRENTICESHIP</Nav.Link>
             </Nav>
         </Navbar>
-        <img src="https://via.placeholder.com/1750x1000.png" style={{ width: '100%' }} />
+        <img src="https://via.placeholder.com/1800x1000.png" style={{ width: '100%' }} />
         <br />
         <br />
-        <Row>
-            <Col><img src="https://via.placeholder.com/800x600.png" style={{ width: '100%', display: 'flex' }} /></Col>
-            <Col><Container style={{ align: 'right' }}>
-                <h3 style={{ textAlign: 'center' }}>Apprenticeship</h3>
+        <Row style={{ fontFamily: 'TimesNewRoman', width: '80%', margin: 'auto' }}>
+            <Col><img src="https://via.placeholder.com/900x600.png" style={{ width: '100%' }} /></Col>
+            <Col><Container style={{ width: '100%', height: '100%', position: 'relative' }}><Container style={{ margin: 0, position: 'absolute', top: '50%', MsTransform: 'translateY(-50%)', transform: 'translateY(-50%)' }}>
+                <h3 style={{ textAlign: 'center' }}><b>Apprenticeship</b></h3>
                 <p>Our founder, Jonathan Tsai, started an apprenticeship program at A&R to train and equip aspiring Christian software engineers with tangible skills. In the SF Bay Area there is an ever-increasing number of people coming out of coding bootcamps trying to transition into software roles from other industries. Unfortunately, opportunities for bootcamp grads have severely declined since 2015 and many people are struggling to find employment after investing substantially into building their coding skills</p>
-            </Container></Col>
+                <br />
+                <Button href="#apprenticeship" variant="outline-dark" style={{ margin: 'auto', display: 'block', width: '35%' }}>LEARN MORE</Button>            
+            </Container></Container></Col>
         </Row>
+        <br /><hr style={{ width: '80%', height: '1px', backgroundColor: '#c8c8c8' }} /><br />
+        <Container style={{ fontFamily: 'TimesNewRoman', width: '80%', margin: 'auto' }}>
+            <h3 style={{ textAlign: 'center' }}><b>Who We Are</b></h3>
+            <p style ={{ textAlign: 'center', width: '60%', margin: 'auto' }}>Awe & Reverence (A&R) is a software company commited to building a God-glorifying, for-profit (read: sustainable), for-Kingdom benefit, cutting-edge Christian software company.</p>
+            <br />
+            <Button href="#about" variant="outline-dark" style={{ margin: 'auto', display: 'block', width: '16%' }}>READ MORE</Button>
+        </Container>
+        <br /><hr style={{ width: '80%', height: '1px', backgroundColor: '#c8c8c8' }} /><br />    
         </>
     )
 }
